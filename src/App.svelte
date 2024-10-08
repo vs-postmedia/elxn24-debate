d9734a25f612a6e25ea6c5abf013739a35d56aa3
Nathan Griffiths <ngriffiths@postmedia.com>
2024-10-08 15:08:53 -0700

    initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -3,64 +3,37 @@
     import { onMount } from 'svelte';
     import { csvParse } from 'd3-dsv';
     import Chart from "$components/Chart.svelte";
-    import Select from "svelte-select"; // https://github.com/rob-balfre/svelte-select
 
     
 
     // DATA
-    // import data from "$data/data.js";
-    import { menuItems } from "$data/menu-items";
     const dataUrl = 'https://raw.githubusercontent.com/ajstarks/dubois-data-portraits/master/challenge/2024/challenge03/data.csv';
 
     // VARIABLES
-    let data, value;
-    const defaultSelectValue = menuItems[0].value;
-
-    // REACTIVE VARIABLES
-    $: value, updateData(value);
-
+    let data;
     async function fetchData(url) {
         const resp = await fetch(url);
         data = await resp.text();
         return csvParse(data);
     }
 
-
-    function updateData(value) {
-        if (!value || !value.value) return;
-
-        console.log(value);
-    }
-
     async function init() {
         // fetch remote data
         data = await fetchData(dataUrl);
         // console.log(data);
-
-        // default display selector value
-		value = defaultSelectValue;
     }
 
     onMount(init);
 </script>
 
 <header>
-    <h1>VS SvelteKit Template</h1>
+    <h1>In their words</h1>
     <p class="subhead">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </header>
 
 <main>
-    <Select items={menuItems}
-        bind:value
-        change={updateData}
-        placeholder="Pick a city..."
-		showChevron="true"
-		listOpen={false}
-    />
-    
     <Chart 
         data={data}
-        value={value}
     />
 </main>
 
@@ -85,22 +58,5 @@
 		margin: 0 auto;
 		max-width: 525px;
 		text-align: center;
-	}
-
-    /* COMBOBOX SELECTOR */
-  	:global(.svelte-select) {
-		margin: 1rem auto !important;
-		max-width: 250px;
-  	}
-  	:global(input:focus) {
-		outline: none;
-  	}
-
-	:global(
-		.svelte-select .selected-item,
-		.svelte-select .item,
-		.svelte-select input
-	) {
-		font-family: 'BentonSansCond-Regular', sans;
 	}
 </style>
