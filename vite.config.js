4d65d6b38dd27eb88ceeb996d68846bed1817c34
Nathan Griffiths <ngriffiths@postmedia.com>
2024-10-08 17:59:09 -0700

    forgot basic plumbing

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/svelte-template/',
+  base: '/elxn24-debate/',
   // base: process.env.MODE === 'production' ? projectPath : '/',
   plugins: [svelte()],
   resolve: {
